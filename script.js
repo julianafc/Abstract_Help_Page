c753f34c5d5d57257e08f9dad84d403734633bae
Juliana <julianafigueirac@gmail.com>
2025-01-14 00:11:00 -0300

    inicio barrinhas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navButtons = document.getElementById("nav-buttons");
+const menuToggle = document.getElementById("barrinhas");  // O botão de menu
+const navButtons = document.getElementById("botoes-hidden"); // O menu oculto
 
+// Adiciona um ouvinte de evento para alternar a classe 'show' quando o botão for clicado
 menuToggle.addEventListener("click", () => {
-    navButtons.classList.toggle("show"); 
+    navButtons.classList.toggle("show"); // Alterna a visibilidade do menu
 });
