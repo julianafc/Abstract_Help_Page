7c17334845aa642d13ae2d97bce1cb471d39a28d
Juliana <julianafigueirac@gmail.com>
2025-01-14 23:32:55 -0300

    melhorando responsividade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const menuToggle = document.getElementById("barrinhas");  // O botão de menu
-const navButtons = document.getElementById("botoes-hidden"); // O menu oculto
+const menuToggle = document.getElementById("barrinhas"); 
+const navButtons = document.getElementById("botoes-hidden");
 
-// Adiciona um ouvinte de evento para alternar a classe 'show' quando o botão for clicado
 menuToggle.addEventListener("click", () => {
-    navButtons.classList.toggle("show"); // Alterna a visibilidade do menu
+    navButtons.classList.toggle("show");
 });
+
